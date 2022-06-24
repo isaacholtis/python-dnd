ad11f4077950057aa1361d0ff67716585ff789f4
Isaac Holt <holtimzws@outlook.com>
2022-06-24 13:46:19 -0600

    Made first run handling better

--- main.py
+++ main.py
@@ -19,6 +19,21 @@ if(os.path.exists("players.json")):
         else:
             print(f"{playerName}'s final score is {playerInfo[player][0]}") 
 else: 
-    print("First run of program, please create players.json and input your players and their initiative dice in dictionary format before running again")
+    print("First run of program")
     # Put a players name and dice in like this {"playername": "playerdice"} seperated by a coma in between different players
-    main.FirstRun({})
+    main.FirstRun({"connor": "3d6+0", "isaac": "2d6+0", "lincoln": "1d8+1"})
+    initiative, playerInfo = main.InterData(main.ReadData())
+    print(f"Initiative for today will be in this order: {initiative}\nHere's how the players rolled: ")
+    for player in initiative:
+        playerName = player
+        playerName = playerName[0].upper() + playerName[1:1000]
+        print(f"{playerName} rolls a {main.playerDiceType[player]}")
+        if playerInfo[player][2] != 0:
+            for roll in playerInfo[player][1]:
+                print(f"{playerName} rolled a {roll}")
+        else:
+            print(f"{player} rolled a {roll}")
+        if playerInfo[player][3] != 0:
+            print(f"{playerName} also adds {playerInfo[player][3]} to their final result, which gives {playerName} a total of {playerInfo[player][0]}")
+        else:
+            print(f"{playerName}'s final score is {playerInfo[player][0]}") 
